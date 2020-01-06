d1465650bb142e8b0a4f62df96b0c236cb968737
SuveshBaskar <suvesh@yellowmessenger.com>
2020-01-06 20:58:20 +0530

    Added Index.html

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.json({ success: true });
+});
+
 app.listen(PORT, () => {
   console.log(`Server started in port: ${PORT}`);
 });

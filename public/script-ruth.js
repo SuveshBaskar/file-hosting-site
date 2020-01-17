0f6832ae6ec88d296e96ecdb2fa4af1cca12b2c4
SuveshBaskar <suvesh@yellowmessenger.com>
2020-01-17 13:43:26 +0530

    Added changes

--- public/script-ruth.js
+++ public/script-ruth.js
@@ -1,6 +1,33 @@
-(function () {
-    window.addEventListener("message", function (event) {
+(function(){
+    function addProductToCart(EAN){
+    console.log(EAN);
+        if (_shopalyst !== undefined) {
+            _shopalyst.addEANToCart(EAN);
+            // Close the chatbot here!
+            if (window.YellowMessengerPlugin !== undefined) {
+            
+       window.YellowMessengerPlugin.toggleChat()
+}
+        } else {
+            // Open up the Shopalyst URL
+            const SHOPALYST_BASE_URL = 'https://sdk.shortlyst.com/ean/';
+            window.location.assign(SHOPALYST_BASE_URL+EAN);
+        }
+    }
+    window.addEventListener("message", function(event){
         var event = JSON.parse(event.data);
+        if (event.event_code === 'custom-event') {
+            switch(event.data.code) {
+                case 'add-to-cart': {
+                    var EANCode = event.data.EAN;
+                    addProductToCart(EANCode);
+                    break;
+                }
+                default : {
+                    console.log(JSON.stringify(event));
+                }
+            }
+        }
         let ymf = document.getElementById('ymFrameHolder')
         let ymc = document.getElementById('ymDivCircle')
         if(event.event_code === "open_bot"){
@@ -8,9 +35,15 @@
             ymf.style.opacity=1
             ymc.classList.add('open')
         }else if(event.event_code === "close_bot"){
+            let notifHTML = `<div class="close" onclick="window.ymCloseNotification(event);">x</div><div style="font-weight: bold;">Tenaga bot</div>`
+            if(!document.getElementById('ym-notification').innerHTML){
+                console.log("Notification is not there")
+                document.getElementById('ym-notification').innerHTML = notifHTML;
+            } else {
+                console.log("Notification is present")
+            }
             ymf.style.display="none"
             ymf.style.opacity=0
         }else{}
-                
-    }, false);
+    });
 })();
